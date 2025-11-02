97b169711619ec333eee344c846bf1697e25cb3f
Aryan_Bhardwaj <bhardwaj03aryan@gmail.com>
2025-11-02 22:35:19 +0530

    feat(server): set up HTTP server with Socket.IO and CORS configuration

--- apps/server/index.ts
+++ apps/server/index.ts
@@ -1,1 +1,15 @@
-const PORT = process.env.PORT || 8080
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const PORT = process.env.PORT || 8080;
+const httpServer = createServer();
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: " * ",
+    methods: ["GET", "POST"],
+  },
+});
+httpServer.listen(PORT, () => {
+  console.log(`server is running at PORT: ${PORT} from apps/server/index.ts`);
+});
