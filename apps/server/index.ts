46ea041e2fbce5d00eed243d2b1b4080cd71f564
Aryan_Bhardwaj <bhardwaj03aryan@gmail.com>
2025-11-14 23:52:51 +0530

    Add socket service and integrate with server
    Introduced a new socketService class in apps/server/services/sokcet.ts to encapsulate Socket.IO server initialization and event handling. Updated index.ts to use the new service for attaching the HTTP server. This refactor improves code organization and prepares for future socket event extensions.

--- apps/server/index.ts
+++ apps/server/index.ts
@@ -1,5 +1,6 @@
 import { createServer } from "http";
 import { Server } from "socket.io";
+import socketService from "./services/sokcet.js";
 
 const PORT = process.env.PORT || 8080;
 const httpServer = createServer();
@@ -10,6 +11,8 @@ const io = new Server(httpServer, {
     methods: ["GET", "POST"],
   },
 });
+const socketServices = new socketService(); // initializing the server with http
+socketServices.io.attach(httpServer); // attaching http server method above
 httpServer.listen(PORT, () => {
   console.log(`server is running at PORT: ${PORT} from apps/server/index.ts`);
 });
---
+++ apps/server/services/sokcet.ts
@@ -0,0 +1,44 @@
+import { Server } from "socket.io";
+
+{
+  /* 
+  this file is used to initialize the socket io 
+  _io --> instance variable 
+
+  */
+}
+
+class socketService {
+  // class allows to create new instance without writting the same code
+  private _io: Server; // this _io is used to do handle realtime comms , is listens and broadcasts the message to everyone on the server
+  constructor() {
+    console.log("new server initailzied");
+    this._io = new Server();
+  }
+
+  get io() {
+    // this is so that private vairabele _io can be accessed ad io outside , TODO: what more benifit is still unkonwn
+    return this._io;
+  }
+
+  public listnerInit() {
+    const io = this.io;
+    console.log("init listener available >>>...");
+    io.on("connect", (socket) => {
+      // 'socket' is specific to the newly- uniqe -connected client
+      console.log(`new connection established `, socket.id);
+      socket.on("event:message", async ({ message }: { message: String }) => {
+        // now a event listener is esatblished for the uniqe connected client
+        console.log(`New Message, ${message}`);
+      });
+    });
+  }
+}
+{
+  /*
+  TODO: 1. get fronend here , 2. add more comments to explain what was done
+  
+  */
+}
+
+export default socketService;
